e4cc695569c858f028f179e9b5cae19e998f8186
yournewgoodname <linda.mcgraw@me.com>
2024-09-11 16:07:12 -0500

    add file to clean-up laravel files

---
+++ bin/laravel-cleanup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Create a new Laravel project in a temporary directory" 
+
+rm -rf app2
+rm -rf app
+rm -rf .env.laravel
+rm -rf bootstrap
+rm -rf database
+rm -rf config
+rm -rf ph-fpm.d
+rm -rf php-fpm.d
+rm -rf public
+rm -rf resources
+rm -rf routes
+rm -rf storage
+rm -rf tests
+rm -rf vendor
+rm -rf .editorconfig
+rm -rf artisan
+rm -rf composer-laravel.json
+rm -rf composer-laravel.lock 
+rm -rf package.json
+rm -rf phpunit.xml
+rm -rf vite.config.js
+rm -rf .gitattributes
